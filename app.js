1897a3ff792fac25fb63c3a743a825d097b9b47d
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2022-01-28 14:23:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     res.status(200).json({
-        message : "Hello developer!",
+        message : "Hello dev!",
         success : true
     })
 })

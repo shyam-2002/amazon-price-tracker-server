88972cf8993203d815708fe8b09e91b4481890d5
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2022-07-28 21:42:36 +0530

    added health check route and removed side-channel module

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -5,4 +5,11 @@ let router = express.Router();
 router.use('/user', require('./log_sign_routes'));
 router.use('/', require('./other_routes'));
 
+router.get('/health', (req, res)=>{
+    res.status(200).json({
+        success : true,
+        messagee : "everything is fine"
+    })
+})
+
 module.exports = router;
